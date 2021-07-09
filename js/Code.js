c75e70bbc8e5abd3c38903340a037a37c8f287bf
Ashwani841 <83075220+Ashwani841@users.noreply.github.com>
2021-07-08 21:19:25 -0700

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,40)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,60)
+    text("Use Capital letters only",100,80)
+    // add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,140)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!", 700,160)
+    text("Use Capital letters only",700,180)
+
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A", 100,240)
+    fill("lightblue")
+    text("Hint: Stores Information !!", 100,260)
+    text("Use Capital letters only",100,280)
+}
